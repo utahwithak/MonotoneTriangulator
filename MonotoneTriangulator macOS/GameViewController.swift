5d0520360472c3ba6ad8ab37f137acd8c816357e
Carl Wieland <cwieland@esri.com>
2020-02-20 11:53:15 -0500

    Initial Commit

---
+++ MonotoneTriangulator macOS/GameViewController.swift
@@ -0,0 +1,31 @@
+//
+//  GameViewController.swift
+//  MonotoneTriangulator macOS
+//
+//  Created by Carl Wieland on 2/20/20.
+//  Copyright © 2020 Datum Apps. All rights reserved.
+//
+
+import Cocoa
+import SpriteKit
+import GameplayKit
+
+class GameViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene.newGameScene()
+        
+        // Present the scene
+        let skView = self.view as! SKView
+        skView.presentScene(scene)
+        
+        skView.ignoresSiblingOrder = true
+        
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+    }
+
+}
+
